51a824eaecfa9591dcf650cf98f5b937c4c1f0e6
laharivishista18 <laharivishistasampangi@gmail.com>
2024-09-23 15:00:04 +0530

    Add files via upload

---
+++ Collection/ArraysClass/EvergentTopicsImpl.java
@@ -0,0 +1,20 @@
+package com.evergent.corejava.Collections.arrays;
+import java.util.Arrays;
+import java.util.List;
+public class EvergentTopicsImpl {
+	
+	public static void main(String[] args) {
+		
+	 
+	List topics=Arrays.asList(new EvergentTopics("Encapsulation :","Inheritance :"," : OOPS"),
+            new EvergentTopics("Checked Exception :","UnChecked Exception :"," ExceptonsHandling"),
+            new EvergentTopics("SET","LIST","Collection"));
+	
+	System.out.println(topics);
+	
+	  List mydata=Arrays.asList(10,20,30);
+	  System.out.println(mydata);
+	  
+	}
+
+}

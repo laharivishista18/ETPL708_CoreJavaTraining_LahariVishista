0b5abced9e802ac8bdf338d7a19dffc2aa330273
laharivishista18 <laharivishistasampangi@gmail.com>
2024-09-23 15:02:37 +0530

    Add files via upload

---
+++ IO/io/IO1_FileDemo.java
@@ -0,0 +1,22 @@
+package com.evergent.corejava.io;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class IO1_FileDemo {
+    public static void main(String[] args) {
+        try {
+            // Attempt to open a file that doesn't exist
+            File file = new File("C:/Users/lahari.sampangi/Downloads/example.txt");
+        	
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            // Handle the FileNotFoundException
+            System.out.println("File not found: " + e.getMessage());
+        }
+    }
+} 
+ 
